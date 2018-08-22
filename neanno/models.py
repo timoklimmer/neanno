c1bf525a65321472876b63a463d91b27042ff4ce
Timo Klimmer <timoklimmer@users.noreply.github.com>
2018-08-22 12:00:45 +0200

    Pandas model

--- neanno/models.py
+++ neanno/models.py
@@ -1,106 +1,33 @@
-# from PyQt5 import QtCore
-from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex
+from PyQt5.QtCore import QAbstractTableModel, QModelIndex, QVariant, Qt
 
-# import pandas as pd
+import pandas as pd
 
 
-class _MyModel(QAbstractTableModel):
-    def __init__(self, data, parent=None):
-        QAbstractTableModel.__init__(self, parent)
-        self.lst = data
-
-    def columnCount(self, parent=QModelIndex()):
-        return len(self.lst[0])
-
-    def rowCount(self, parent=QModelIndex()):
-        return len(self.lst)
+class _TextModel(QAbstractTableModel):
+    def __init__(self, df=pd.DataFrame(), parent=None):
+        super().__init__(parent=parent)
+        self._df = df
 
     def data(self, index, role=Qt.DisplayRole):
-        row = index.row()
-        col = index.column()
+        return (
+            self._df.ix[index.row(), index.column()]
+            if index.isValid()
+            else QVariant()
+        )
 
-        if role == Qt.EditRole:
-            return self.lst[row][col]
-        elif role == Qt.DisplayRole:
-            return self.lst[row][col]
-
-    def flags(self, index):
-        flags = super(_MyModel, self).flags(index)
-
-        if index.isValid():
-            flags |= Qt.ItemIsEditable
-            flags |= Qt.ItemIsDragEnabled
-        else:
-            flags = Qt.ItemIsDropEnabled
-
-        return flags
-
-    def setData(self, index, value, role=Qt.EditRole):
-
-        if not index.isValid() or role != Qt.EditRole:
-            return False
-
-        self.lst[index.row()][index.column()] = value
+    def setData(self, index, value, role):
+        row = self._df.index[index.row()]
+        col = self._df.columns[index.column()]
+        self._df.set_value(row, col, value)
         self.dataChanged.emit(index, index)
         return True
 
+    def rowCount(self, parent=QModelIndex()):
+        return len(self._df.index)
 
-# class PandasModel(QtCore.QAbstractTableModel):
-#     def __init__(self, df=pd.DataFrame(), parent=None):
-#         QtCore.QAbstractTableModel.__init__(self, parent=parent)
-#         self._df = df
+    def columnCount(self, parent=QModelIndex()):
+        return len(self._df.columns)
 
-#     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-#         if role != QtCore.Qt.DisplayRole:
-#             return QtCore.QVariant()
-
-#         if orientation == QtCore.Qt.Horizontal:
-#             try:
-#                 return self._df.columns.tolist()[section]
-#             except (IndexError,):
-#                 return QtCore.QVariant()
-#         elif orientation == QtCore.Qt.Vertical:
-#             try:
-#                 # return self.df.index.tolist()
-#                 return self._df.index.tolist()[section]
-#             except (IndexError,):
-#                 return QtCore.QVariant()
-
-#     def data(self, index, role=QtCore.Qt.DisplayRole):
-#         if role != QtCore.Qt.DisplayRole:
-#             return QtCore.QVariant()
-
-#         if not index.isValid():
-#             return QtCore.QVariant()
-
-#         return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
-
-#     def setData(self, index, value, role):
-#         row = self._df.index[index.row()]
-#         col = self._df.columns[index.column()]
-#         if hasattr(value, "toPyObject"):
-#             # PyQt4 gets a QVariant
-#             value = value.toPyObject()
-#         else:
-#             # PySide gets an unicode
-#             dtype = self._df[col].dtype
-#             if dtype != object:
-#                 value = None if value == "" else dtype.type(value)
-#         self._df.set_value(row, col, value)
-#         self.dataChanged.emit(index, index)
-#         return True
-
-#     def rowCount(self, parent=QtCore.QModelIndex()):
-#         return len(self._df.index)
-
-#     def columnCount(self, parent=QtCore.QModelIndex()):
-#         return len(self._df.columns)
-
-#     def sort(self, column, order):
-#         colname = self._df.columns.tolist()[column]
-#         self.layoutAboutToBeChanged.emit()
-#         self._df.sort_values(
-#             colname, ascending=order == QtCore.Qt.AscendingOrder, inplace=True
-#         )
-#         self._df.reset_index(inplace=True, drop=True)
-#         self.layoutChanged.emit()
+    def flags(self, index):
+        return super().flags(index) | Qt.ItemIsEditable
+        
