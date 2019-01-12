80a39a284a00e6e2a2bacf90f383f761f7f0ed97
Timo Klimmer <timo.klimmer@live.de>
2019-01-12 12:05:59 +0100

    Intermediate

--- neanno/ui/syntaxhighlighters.py
+++ neanno/ui/syntaxhighlighters.py
@@ -9,84 +9,87 @@ class TextEditHighlighter(QSyntaxHighlighter):
 
     format_info = {}
 
+    ENTITY_NAME_BACKGROUND_COLOR = "lightgrey"
+    ENTITY_NAME_FOREGROUND_COLOR = "black"
+    PARENT_TERMS_FOREGROUND_COLOR = "darkgrey"
+    PARENT_TERMS_BACKGROUND_COLOR = "black"
+
     def __init__(self, parent, named_definitions):
         super(TextEditHighlighter, self).__init__(parent)
 
         # populate format infos
         self.format_info = {
             "standalone_key_term": {
-                "backcolor": config.key_terms_backcolor,
-                "forecolor": config.key_terms_forecolor,
-                "show_postfix": False,
+                "main_backcolor": config.key_terms_backcolor,
+                "main_forecolor": config.key_terms_forecolor,
+                "show_entity_name": False,
+                "show_key_terms": False
             },
             "parented_key_term": {
-                "backcolor": config.key_terms_backcolor,
-                "forecolor": config.key_terms_forecolor,
-                "show_postfix": True,
+                "main_backcolor": config.key_terms_backcolor,
+                "main_forecolor": config.key_terms_forecolor,
+                "show_entity_name": False,
+                "show_key_terms": True
             },
             "standalone_named_entity": {},
+            "parented_named_entity": {},
         }
         for named_definition in named_definitions:
             self.format_info["standalone_named_entity"][named_definition.code] = {
-                "backcolor": named_definition.backcolor,
-                "forecolor": named_definition.forecolor,
-                "show_postfix": True,
+                "main_backcolor": named_definition.backcolor,
+                "main_forecolor": named_definition.forecolor,
+                "show_entity_name": True,
+                "show_key_terms": False
+            }
+            self.format_info["parented_named_entity"][named_definition.code] = {
+                "main_backcolor": named_definition.backcolor,
+                "main_forecolor": named_definition.forecolor,
+                "show_entity_name": True,
+                "show_key_terms": True
             }
 
     def highlightBlock(self, text):
         for annotation in extract_annotations_as_generator(text):
             # get common format infos
             format_info_to_apply = (
-                self.format_info[annotation["type"]][annotation["entity_name"]]
-                if annotation["type"] == "standalone_named_entity"
-                else self.format_info[annotation["type"]]
+                self.format_info[annotation["type"]]
+                if "named_entity" not in annotation["type"]
+                else self.format_info[annotation["type"]][annotation["entity_name"]]
             )
-            backcolor = format_info_to_apply["backcolor"]
-            forecolor = format_info_to_apply["forecolor"]
-            show_postfix = format_info_to_apply["show_postfix"]
-            postfix_background_color = "lightgrey"
-            postfix_foreground_color = "black"
+            main_backcolor = format_info_to_apply["main_backcolor"]
+            main_forecolor = format_info_to_apply["main_forecolor"]
+            show_entity_name = format_info_to_apply["show_entity_name"]
+            show_parent_terms = format_info_to_apply["show_parent_terms"]
 
-            # assemble formats
-            open_paren_format = self.get_text_char_format(
-                backcolor, backcolor, int(100 / 3)
-            )
-            term_format = self.get_text_char_format(backcolor, forecolor)
-            pipe_format = self.get_text_char_format(backcolor, backcolor, int(100 / 3))
-            if show_postfix:
-                type_format = self.get_text_char_format(
-                    postfix_background_color, postfix_background_color, int(100 / 4)
-                )
-                postfix_text_format = self.get_text_char_format(
-                    postfix_background_color,
-                    postfix_foreground_color,
-                    None,
-                    "Segoe UI",
-                    QFont.Bold,
-                    10,
-                )
-                closing_paren_format = self.get_text_char_format(
-                    postfix_background_color, postfix_background_color, int(100 / 4)
-                )
-            else:
-                type_format = self.get_text_char_format(backcolor, backcolor, 1)
-                postfix_text_format = self.get_text_char_format(backcolor, backcolor, 1)
-                closing_paren_format = self.get_text_char_format(backcolor, backcolor, 1)
+            term_text_char_format = self.get_text_char_format(main_backcolor, main_forecolor)
+            
+
+
+
 
             # set formats
             # opening parenthesis
+            open_paren_format = self.get_text_char_format(
+                main_backcolor, main_backcolor, int(100 / 3)
+            )
             from_position = annotation["start_gross"]
             length = len("´<`")
             self.setFormat(from_position, length, open_paren_format)
             # term
+            term_format = self.get_text_char_format(main_backcolor, main_forecolor)
             from_position += length
             length = len(annotation["term"])
             self.setFormat(from_position, length, term_format)
             # pipe
+            pipe_format = self.get_text_char_format(
+                main_backcolor, main_backcolor, int(100 / 3)
+            )
             from_position += length
             length = len("´|`")
             self.setFormat(from_position, length, pipe_format)
             # type
+            type_color = "red"
+            type_format = self.get_text_char_format(type_color, type_color, int(100 / 4))
             from_position += length
             length = len("XX ")
             self.setFormat(from_position, length, type_format)
@@ -119,4 +122,9 @@ class TextEditHighlighter(QSyntaxHighlighter):
             result.setFontWeight(font_weight)
         if font_size is not None:
             result.setFontPointSize(font_size)
+        return result
+
+    def get_hide_chars_format(self, text_char_format):
+        result = text_char_format
+        result.setForeground(result.background())
         return result
--- neanno/utils/text.py
+++ neanno/utils/text.py
@@ -7,12 +7,14 @@ ANNOTATION_TYPES = [
     "standalone_key_term",
     "parented_key_term",
     "standalone_named_entity",
+    "parented_named_entity",
 ]
 
 TINY_TO_LONG_ANNOTATION_TYPE_MAPPING = {
     "SK": "standalone_key_term",
     "PK": "parented_key_term",
     "SN": "standalone_named_entity",
+    "PN": "parented_named_entity",
 }
 
 ANNOTATION_REGEX = re.compile(
@@ -20,9 +22,15 @@ ANNOTATION_REGEX = re.compile(
         ´\<`
         (?P<term>.*?)
         ´\|`
-        (?P<type_tiny>((?P<standalone_key_term>SK)|(?P<parented_key_term>PK)|(?P<standalone_named_entity>SN)))
-        (?(parented_key_term)\s(?P<parent_terms>.+?))
-        (?(standalone_named_entity)\s(?P<entity_name>.+?))
+        (?P<type_tiny>(
+              (?P<standalone_key_term>SK)
+            | (?P<parented_key_term>PK)
+            | (?P<standalone_named_entity>SN)
+            | (?P<parented_named_entity>SN))
+        )
+        (?(parented_key_term)\s(?P<parent_terms_pk>.*?))
+        (?(standalone_named_entity)\s(?P<entity_name_sn>.+?))
+        (?(parented_named_entity)\s(?P<entity_name_pn>.+?\s(?P<parent_terms_pn>.*?)))
         ´\>`
     """,
     flags=re.DOTALL | re.VERBOSE,
@@ -33,6 +41,11 @@ def extract_annotations_as_generator(
     annotated_text, types_to_extract=None, entity_names_to_extract=None
 ):
     """ Yields all annotations from an annotated text as a list."""
+
+    def extract_normalized_parent_terms(parent_terms):
+        return ", ".join(
+            [parent_terms.strip() for parent_term in (parent_terms or "").split(",")]
+        )
 
     # ensure that types_to_extract has valid entries
     ensure_items_within_set(types_to_extract, ANNOTATION_TYPES, True)
@@ -48,18 +61,20 @@ def extract_annotations_as_generator(
         if types_to_extract is not None and annotation["type"] not in types_to_extract:
             continue
         if annotation["type"] == "standalone_named_entity":
-            annotation["entity_name"] = match.group("entity_name")
+            annotation["entity_name"] = match.group("entity_name_sn")
             if (
                 entity_names_to_extract is not None
                 and annotation["entity_name"] not in entity_names_to_extract
             ):
                 continue
         if annotation["type"] == "parented_key_term":
-            annotation["parent_terms"] = ", ".join(
-                [
-                    parent_term.strip()
-                    for parent_term in (match.group("parent_terms") or "").split(",")
-                ]
+            annotation["parent_terms"] = extract_normalized_parent_terms(
+                match.group("parent_terms_pk")
+            )
+        if annotation["type"] == "parented_named_entity":
+            annotation["entity_name"] = match.group("entity_name_pn")
+            annotation["parent_terms"] = extract_normalized_parent_terms(
+                match.group("parent_terms_pn")
             )
         annotation["start_net"] = len(
             remove_all_annotations_from_text(annotated_text[: match.start()])
@@ -104,7 +119,7 @@ def extract_annotations_as_text(
                 annotation.lower() for annotation in external_annotations_to_add
             ]:
                 result_list.append(annotation_to_add)
-        # parented key terms
+        # parented key term
         if annotation["type"] == "parented_key_term":
             parent_terms = []
             for parent_term in set(annotation["parent_terms"].split(",")):
@@ -116,12 +131,27 @@ def extract_annotations_as_text(
                 ]:
                     parent_terms.append(annotation_to_add)
             result_list.extend(sorted(parent_terms))
-        # named entity
+        # standalone named entity
         if annotation["type"] == "standalone_named_entity":
             annotation_to_add = (
                 "{}:{}".format(annotation["entity_name"].lower(), annotation["term"])
                 if include_entity_names
                 else annotation["term"]
+            )
+            if annotation_to_add.lower() not in [
+                annotation.lower() for annotation in result_list
+            ] and annotation_to_add.lower() not in [
+                annotation.lower() for annotation in external_annotations_to_add
+            ]:
+                result_list.append(annotation_to_add)
+        # parented named entity
+        if annotation["type"] == "parented_named_entity":
+            annotation_to_add = (
+                "{}:{}".format(
+                    annotation["entity_name"].lower(), annotation["parent_terms"]
+                )
+                if include_entity_names
+                else annotation["parent_terms"]
             )
             if annotation_to_add.lower() not in [
                 annotation.lower() for annotation in result_list
@@ -145,6 +175,7 @@ def extract_annotations_by_type(
         "standalone_key_terms": "standalone_key_terms",
         "parented_key_terms": "parented_key_terms",
         "standalone_named_entities": "standalone_named_entities",
+        "parented_named_entities": "parented_named_entities",
     },
 ):
     """ Returns all annotations and their position ranges from an annotated text."""
@@ -155,25 +186,29 @@ def extract_annotations_by_type(
     # get the annotations dictionary
     annotations = {}
     # standalone key terms
-    standalone_key_terms = extract_annotations_as_list(
-        annotated_text, types_to_extract=["standalone_key_term"]
-    )
-    if len(standalone_key_terms) > 0:
-        annotations[list_aliases["standalone_key_terms"]] = standalone_key_terms
+    if "standalone_key_term" in types_to_extract:
+        annotations[list_aliases["standalone_key_terms"]] = extract_annotations_as_list(
+            annotated_text, types_to_extract=["standalone_key_term"]
+        )
     # parented key terms
-    parented_key_terms = extract_annotations_as_list(
-        annotated_text, types_to_extract=["parented_key_term"]
-    )
-    if len(parented_key_terms) > 0:
-        annotations[list_aliases["parented_key_terms"]] = parented_key_terms
+    if "parented_key_term" in types_to_extract:
+        annotations[list_aliases["parented_key_terms"]] = extract_annotations_as_list(
+            annotated_text, types_to_extract=["parented_key_term"]
+        )
     # standalone named entities
-    standalone_named_entities = extract_annotations_as_list(
-        annotated_text, types_to_extract=["standalone_named_entity"]
-    )
-    if len(standalone_named_entities) > 0:
+    if "standalone_named_entity" in types_to_extract:
         annotations[
             list_aliases["standalone_named_entities"]
-        ] = standalone_named_entities
+        ] = extract_annotations_as_list(
+            annotated_text, types_to_extract=["standalone_named_entity"]
+        )
+    # parented named entities
+    if "parented_named_entity" in types_to_extract:
+        annotations[
+            list_aliases["parented_named_entities"]
+        ] = extract_annotations_as_list(
+            annotated_text, types_to_extract=["parented_named_entity"]
+        )
 
     # return result
     return (plain_text, annotations)
@@ -189,7 +224,7 @@ def extract_annotations_for_spacy_ner(annotated_text, entity_names_to_extract=No
     annotations = []
     for annotation in extract_annotations_as_generator(
         annotated_text,
-        types_to_extract=["standalone_named_entity"],
+        types_to_extract=["standalone_named_entity", "parented_named_entity"],
         entity_names_to_extract=entity_names_to_extract,
     ):
         annotations.append(
@@ -215,7 +250,7 @@ def remove_all_annotations_from_text(annotated_text):
     """Removes all annotations from the specified text."""
 
     new_text = re.sub(
-        r"´\<`(.*?)´\|`(((PK|SN) .+?)|(SK))´\>`",
+        r"´\<`(.*?)´\|`(((PK|SN|PN) .+?)|(SK))´\>`",
         lambda match: match.group(1),
         annotated_text,
         flags=re.DOTALL,
@@ -225,8 +260,9 @@ def remove_all_annotations_from_text(annotated_text):
 
 def mask_annotations(text):
     """Masks all annotations, eg. to avoid that terms which are already annotated are annotated again."""
+
     return re.sub(
-        r"´\<`.*?´\|`(SK|PK .*?|SN .*?)´\>`",
+        r"´\<`.*?´\|`(SK|PK|PN .*?|SN .*?)´\>`",
         lambda match: "@neanno_masked_annotation:{}@".format(
             base64.b64encode(match.group().encode("utf-8")).decode()
         ),
@@ -236,6 +272,7 @@ def mask_annotations(text):
 
 def unmask_annotations(text_with_masked_annotations):
     """Reverts a previous masking of all annotations."""
+
     return re.sub(
         r"@neanno_masked_annotation:(?P<base64string>.*?)@",
         lambda match: base64.b64decode(match.group("base64string")).decode(),
@@ -245,10 +282,13 @@ def unmask_annotations(text_with_masked_annotations):
 
 def extract_named_entities_distribution(annotated_text):
     """ Computes the types and frequencies of named entities in the specified text."""
+    
     result = {}
-    find_entities_pattern = r"´\<`(?P<term>.+?)´\|`SN (?P<label>.+?)´\>`"
-    for entity in re.findall(find_entities_pattern, annotated_text):
-        entity_code = entity[1]
+    for entity_annotation in extract_annotations_as_generator(
+        annotated_text,
+        types_to_extract=["standalone_named_entity", "parented_named_entity"],
+    ):
+        entity_code = entity_annotation["entity_name"]
         if entity_code not in result:
             result[entity_code] = 0
         result[entity_code] += 1
