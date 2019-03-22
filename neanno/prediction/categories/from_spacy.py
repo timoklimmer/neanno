9e9704111c08962488a59e86c060b5b616c8d08c
Timo Klimmer <timoklimmer@users.noreply.github.com>
2019-03-22 20:19:17 +0100

    Initial category prediction (no tested yet)

--- neanno/prediction/categories/from_spacy.py
+++ neanno/prediction/categories/from_spacy.py
@@ -9,8 +9,8 @@ from neanno.prediction.predictor import Predictor
 from neanno.utils.text import extract_annotations_for_spacy_ner, replace_from_to
 
 
-class FromSpacyNamedEntitiesPredictor(Predictor):
-    """ Trains and uses a spacy model to predict named entities. """
+class FromSpacyCategoriesPredictor(Predictor):
+    """ Trains and uses a spacy model to predict text categories. """
 
     source_model = None
     target_model_directory = None
@@ -58,51 +58,57 @@ class FromSpacyNamedEntitiesPredictor(Predictor):
         entity_codes_to_train,
         signals,
     ):
-        # ensure and get the ner pipe from the spacy model
-        if "ner" not in self.spacy_model.pipe_names:
-            self.spacy_model.add_pipe(self.spacy_model.create_pipe("ner"), last=True)
-        ner_pipe = self.spacy_model.get_pipe("ner")
-        # ensure we have all relevant named entities in the model
-        for entity_code_to_train in entity_codes_to_train:
-            ner_pipe.add_label(entity_code_to_train)
+        # ensure and get the textcat pipe from the spacy model
+        if "textcat" not in self.spacy_model.pipe_names:
+            textcat_pipe = self.spacy_model.create_pipe(
+                "textcat",
+                config={"architecture": "simple_cnn", "exclusive_classes": True},
+            )
+            self.spacy_model.add_pipe(textcat_pipe, last=True)
+        textcat_pipe = self.spacy_model.get_pipe("textcat")
+        # ensure we have all categories in the model
+        for category_to_train in categories_to_train:
+            textcat_pipe.add_label(category_to_train)
         # prepare the training set
         trainset = (
-            dataset[dataset[is_annotated_column] == True][text_column]
-            .map(
-                lambda annotated_text: extract_annotations_for_spacy_ner(
-                    annotated_text, entity_codes_to_train
-                )
+            dataset[dataset[is_annotated_column] == True]
+            .apply(
+                lambda row: (
+                    row["text"],
+                    {
+                        "cats": {
+                            category: (category in [row["categories"].split("|")])
+                            for category in categories_to_train
+                        }
+                    },
+                ),
+                axis=1,
             )
             .tolist()
         )
         # do the training
         # note: there is certainly room for improvement, maybe switching to spacy's CLI
         #       which seems the recommendation by the spacy authors
-        signals.message.emit("Training NER model...")
-        n_iterations = 10
-        # note: this removes the unnamed vectors warning, TBD if needs changes
-        self.spacy_model.vocab.vectors.name = "spacy_pretrained_vectors"
-        optimizer = self.spacy_model.begin_training()
-        other_pipes = [pipe for pipe in self.spacy_model.pipe_names if pipe != "ner"]
-        with self.spacy_model.disable_pipes(*other_pipes):
+        signals.message.emit("Training categories model...")
+        n_iterations = 20
+        # get names of other pipes to disable them during training
+        other_pipes = [
+            pipe for pipe in self.spacy_model.pipe_names if pipe != "textcat"
+        ]
+        with self.spacy_model.disable_pipes(*other_pipes):  # only train textcat
+            optimizer = self.spacy_model.begin_training()
             for iteration in range(n_iterations):
-                random.shuffle(trainset)
+                signals.message.emit("Iteration {}...".format(iteration))
                 losses = {}
                 batches = minibatch(trainset, size=compounding(4.0, 32.0, 1.001))
                 for batch in batches:
                     texts, annotations = zip(*batch)
                     self.spacy_model.update(
-                        texts, annotations, sgd=optimizer, drop=0.35, losses=losses
+                        texts, annotations, sgd=optimizer, drop=0.2, losses=losses
                     )
-                signals.message.emit(
-                    "Iteration: {}, loss: {}".format(iteration, losses["ner"])
-                )
 
-        # compute precision/recall for each entity code
-        # TODO: dataset might have changed meanwhile, need to use a frozen dataset for evaluation
-        # TODO: consider using a common eval, not only in this class
-        # TODO: think about if this is the right place (esp. for on-the-fly training)
-        # use compute_ner_metrics_from_actual_predicted_columns() function from neanno.utils.metrics
+        # compute precision/recalls
+        # TODO: complete
 
         # save model to output directory
         if self.target_model_directory is not None:
@@ -113,19 +119,9 @@ class FromSpacyNamedEntitiesPredictor(Predictor):
             self.spacy_model.meta["name"] = self.target_model_name
             self.spacy_model.to_disk(output_dir)
 
-    def predict_inline_annotations(self, text, language="en-US"):
+    def predict_text_categories(self, text, language="en-US"):
         if self.spacy_model:
-            # TODO: add parent terms
-            result = text
-            doc = self.spacy_model(result)
-            shift = 0
-            for ent in doc.ents:
-                old_result_length = len(result)
-                result = replace_from_to(
-                    result,
-                    ent.start_char + shift,
-                    ent.end_char + shift,
-                    "`{}``SN``{}`´".format(ent.text, ent.label_),
-                )
-                shift += len(result) - old_result_length
-            return result
+            doc = self.spacy_model(text)
+            return doc.cats
+        else:
+            return []
